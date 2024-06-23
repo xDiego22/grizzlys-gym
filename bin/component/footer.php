e0defb429baeffec6e24bc29476f30b2f6451678
diego <diegoaguilar221202@gmail.com>
2024-06-23 00:23:34 -0430

    modificacion de mensaje de footer

--- bin/component/footer.php
+++ bin/component/footer.php
@@ -1,7 +1,7 @@
 <footer class="py-4 bg-light mt-auto">
     <div class="container-fluid px-4">
         <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted">Copyright &copy; Your Website <span id="year"></span> </div>
+            <div class="text-muted">Copyright &copy; Grizzlys Gym <span id="year"></span> </div>
 
         </div>
     </div>
