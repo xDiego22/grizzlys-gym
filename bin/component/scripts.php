924b055f6ead0e4925317128949dca2b069be0f7
diego <diegoaguilar221202@gmail.com>
2024-06-23 00:16:17 -0430

    add scripst de librerias de charts e iconos

--- bin/component/scripts.php
+++ bin/component/scripts.php
@@ -4,3 +4,5 @@
 <script type="text/javascript" src="assets/js/librerias/bs-stepper.min.js"></script>
 <script type="text/javascript" src="assets/js/librerias/datatables.min.js"></script>
 <script type="text/javascript" src="assets/js/scripts.js"></script>
+<script type="text/javascript" src="assets/fa/all.min.js"></script>
+<script type="text/javascript" src="assets/js/librerias/chart.js"></script>
